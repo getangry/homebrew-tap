9311f9539d44c37918d61806ed290afe97774cf3
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2025-08-21 23:14:27 -0400

    Fix sha256 format in md.rb

--- Formula/md.rb
+++ Formula/md.rb
@@ -2,7 +2,7 @@ class Md < Formula
   desc "Terminal-based markdown viewer"
   homepage "https://github.com/getangry/md"
   url "https://github.com/getangry/md/releases/download/v1.0.0/md-v1.0.0-darwin-arm64.tar.gz"
-  sha256 "sha256:5f5b5acae2b2b65b1d23baaeaa6f29fb6612761774272b926100301fbfae2bd7"
+  sha256 "5f5b5acae2b2b65b1d23baaeaa6f29fb6612761774272b926100301fbfae2bd7"
   license "MIT"
 
   depends_on "go" => :build
